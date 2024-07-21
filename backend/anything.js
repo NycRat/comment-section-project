084d2c68ca9171306d788d7fa188caa94e7de9c5
Eric Xiao <73317463+NycRat@users.noreply.github.com>
2024-07-20 18:07:41 -0700

    inital commit

---
+++ backend/anything.js
@@ -0,0 +1,43 @@
+import express, { text } from "express"; //!
+
+import cors from "cors";
+const app = express();
+const port = 3600;
+
+app.use(cors());
+app.use(text());
+
+class Comment {
+    id = undefined;
+    name = "";
+    comment = "";
+    replyto = null;
+}
+
+let commentSection = [];
+
+app.get("/", (request, response) => {
+    response.send("Hello World!");
+});
+
+app.post("/post", (request, response) => {
+    let comment = new Comment();
+    comment.id = commentSection.length;
+    comment.name = "Abraham";
+    comment.comment = JSON.stringify(request.body);
+
+    commentSection.push(comment);
+
+    // console.log(toString(commentSection));
+    console.log(commentSection);
+    response.sendStatus(201);
+});
+
+// should be a get instead of post but whateverrrrrrr
+app.post("/get", (request, response) => {
+    response.send(JSON.stringify(commentSection));
+});
+
+app.listen(port, () => {
+    console.log(`app listening on port ${port}`);
+});
---
+++ frontend/script.js
@@ -0,0 +1,4 @@
+const res = await fetch("http://localhost:3600/post", {
+    method: "POST",
+    body: "threeeeeeeeeeeeeeee",
+});
