c9bc47db2abbd7a6abd5929ef425880f458242ef
cole <chloe.peiying.lin@gmail.com>
2024-07-21 15:32:59 -0700

    .

--- frontend/script.js
+++ frontend/script.js
@@ -1,4 +1,32 @@
-const res = await fetch("http://localhost:3600/post", {
-    method: "POST",
-    body: "threeeeeeeeeeeeeeee",
-});
+let commentsDiv = document.getElementById("comments");
+let serverUrl = "http://localhost:3600";
+
+const getComments = async () => {
+    const res = await fetch(`${serverUrl}/get`, {
+        method: "POST",
+    });
+
+    return res
+};
+
+const putComments = async () => {
+    try {
+        const res = await getComments();
+        const comments = await res.json();
+        console.debug(comments);
+        commentsDiv.append(comments);
+    } catch (error) {
+        console.error("404 youre a dumbass");
+    }
+};
+
+putComments();
+
+const postComment = async (comment) => {
+    const res = await fetch(`${serverUrl}/post`, {
+        method: "POST",
+        body: JSON.stringify(comment),
+    });
+
+    return res
+};
